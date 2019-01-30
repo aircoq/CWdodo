c9d91f37e59f06ae796db9fab289482ebd7ceec9
aircoq <wangq1ng@qq.com>
2019-01-30 23:31:23 +0800

    完成预约服务

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -67,34 +67,24 @@ class IndexController extends BaseController
 
 
     # 发起预约服务
-    public function store(Request $request,Pet $pet,Appointment $appointment)
+    public function makeAppointment(Request $request,Pet $pet,Appointment $appointment)
     {
-        $data = $request->only('appointment_type','user_id','user_name','sex', 'user_phone', 'pet_id','is_pickup','province','city','district','address','from_way','start_at','end_at','food_id','mark_up');
+        $data = $request->only('appointment_type','user_phone', 'pet_id','is_pickup','province','city','district','address','start_at','end_at','food_type');
         $role = [
             'appointment_type' => 'required|in:0,1,2,3',
-            'user_id' => 'required|integer',
-            'user_name' => 'required|string|between:2,15',
-            'sex' => 'required|in:0,1',
             'user_phone'=> 'required|digits:11',
-            'pet_id' => 'required|integer',
+            'pet_id' => 'required|exists:pet,id',
             'is_pickup' => 'required|in:0,1',
             'province' => 'nullable|required_if:is_pickup,1|string|between:2,15',
             'city' => 'nullable|required_if:is_pickup,1|string|between:2,15',
             'district' => 'nullable|required_if:is_pickup,1|string|between:2,15',
             'address' => 'nullable|required_if:is_pickup,1|string|between:3,35',
-            'from_way' => 'nullable|string|between:0,50',
             'start_at' => 'required|date|after:now',
             'end_at' => 'nullable|required_if:appointment_type,0|date|after:start_at',
-            'food_id' => 'nullable|required_if:appointment_type,0|exists:food,id',
-            'mark_up' => 'nullable|string|between:0,200',
+            'food_type' => 'nullable|required_if:appointment_type,0|in:0,1',//0自带 1默认
         ];
         $message = [
             'appointment_type.in' => '选择服务类型不正确',
-            'user_id.exists' => '用户不存在',
-            'user_name.required' => '用户名不能为空',
-            'user_name.string' => '用户长度为2到15位的英文、数字组成',
-            'user_name.between' => '用户长度为2到15位的英文、数字组成',
-            'sex.in' => '性别格式不合法',
             'user_phone.required' => '手机号码不能为空',
             'user_phone.digits' => '手机号码不正确',
             'pet_id.integer' => '宠物不存在',
@@ -107,26 +97,30 @@ class IndexController extends BaseController
             'district.required_if' => '接送地址不能空',
             'address.between' => '详细地址最长为35个字节',
             'address.required_if' => '接送地址不能空',
-            'from_way.between' => '预约途径最长为50个字节',
             'start_at.required' => '预约服务时间不能为空',
             'start_at.date' => '预约服务时间格式不正确',
             'end_at.required_if' => '寄养结束时间不能为空',
             'end_at.date' => '预约服务时间格式不正确',
             'end_at.after' => '寄养结束时间不能小于开始时间',
-            'food_id.required_if' => '宠物食品不能为空',
-            'food_id.exists' => '宠物食品不存在',
-            'mark_up.string' => '备注不正确',
-            'mark_up.between' => '备注最大100个字节',
+            'food_type.required_if' => '宠物食品不能为空',
+            'food_type.in' => '宠物食品类型不正确',
         ];
         $validator = Validator::make( $data, $role, $message );
         if( $validator->fails() ){
-            return ['status' => "fail", 'msg' => $validator->messages()->first()];
+            return ['status' => "0", 'msg' => $validator->messages()->first()];
         }
+        #获取当前用户信息
+        $user_now = Redis::get($data['session_key']);
+        $user_now = json_decode($user_now,true);
+        $data['user_id'] = $user_now['id'];
+        $data['user_name'] = $user_now['nickname'];
+        $data['sex'] = $user_now['sex'];
         # 核对用户与宠物的对应关系
         $tf = $pet->where('id',$data['pet_id'])->where('user_id',$data['user_id'])->first();
         if(empty($tf)){
             return ['status' => "fail", 'msg' => '请核对填写宠物信息是否正确'];
         }
+        $data['pet_name'] = $tf['pet_name'];
         # 如果接送 生成坐标 核对填写的地址
         if($data['is_pickup'] == 1){
             $get_adr_info = getGaoMapInfo($data['address'],$data['city']);
@@ -155,16 +149,17 @@ class IndexController extends BaseController
         $data['start_at'] = strtotime($data['start_at']);
         if($data['appointment_type'] != 0){ //如果是寄养
             unset($data['end_at']);
-            unset($data['food_id']);
+            unset($data['food_type']);
         }else{
             $data['end_at'] = strtotime($data['end_at']);
         }
+        $data['from_way'] = 'api';//手机端接口
         $res = $appointment->create($data);
         if ($res) { // 如果添加数据成功，则返回列表页
             event(new AppointmentCreated($res));
-            return ['status' => "success", 'msg' => '预约成功'];
+            return ['status' => "1", 'msg' => '预约成功'];
         }else{
-            return ['status' => 'fail', 'msg' => '预约失败'];
+            return ['status' => '0', 'msg' => '预约失败'];
         }
     }
 
--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ $api->version('v1', function ($api) {
     $api->get('user/login', 'App\Http\Controllers\Api\UserController@login');
     $api->get('index/my_order', 'App\Http\Controllers\Api\IndexController@MyOrder');//查看我的订单
     $api->get('user/band', 'App\Http\Controllers\Api\UserController@bandPhone');//小程序绑定手机
+    $api->get('appointment', 'App\Http\Controllers\Api\IndexController@makeAppointment');//小程序绑定手机
 
 
 });
